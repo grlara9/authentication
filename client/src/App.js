401d85b519724c7fb792b1d6a02f6dd159b658f6
Guillermo Lara <grlara9@gmail.com>
2020-09-06 11:36:39 -0700

    got user page working

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-do
 import './App.css';
 
 import Users from './user/pages/Users';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
     <Router>
+      <MainNavigation />
+      <main>
       <Switch>
         <Route path="/" exact><Users /></Route>
         
         <Redirect to="/" />
       </Switch>
+      </main>
     </Router>
   );
 }
--- client/src/user/components/userItem.js
+++ client/src/user/components/userItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Avatar from '../../shared/components/UIElements/Avatar';
-import Card from '../../shared/components/UIElements/Card';
-import './UserItem.css';
+import Avatar from '../../shared/components/UIElements/Avatar'
+import Card from '../../shared/components/UIElements/Card'
+//import './UserItem.css';
 
 
-const UserItem = props => {
+const UserItem = props => { 
   return (
     <li className="user-item">
       <Card className="user-item__content">
