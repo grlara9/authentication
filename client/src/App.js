3df07240c3d2c76d2fe4ea5cbb8db02aa97f09b2
Guillermo Lara <grlara9@gmail.com>
2020-09-06 13:22:11 -0700

    working on main header and main navigation added css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
-import './App.css';
+
 
 import Users from './user/pages/Users';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
