43f1e0035739672e2fa44af07de5fa3af126aebf
Guillermo Lara <grlara9@gmail.com>
2020-12-29 20:39:16 -0800

    created user controllers and set up routes for authentication

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
 
 const PORT = 3000;
 
 
 app.use('/api/places', placesRoutes); 
+
+app.use('/api/users', usersRoutes);
 
 app.listen(PORT, ()=>{
     console.log("Listeningon port " + PORT)
